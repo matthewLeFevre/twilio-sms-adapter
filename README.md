847f1ab6c4c5d1a0e59252adb1f9194ef775f433
Matthew LeFevre <matthewlefevre95@gmail.com>
2022-03-28 09:08:05 -0600

    Updated licensing and readme.

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Starter Typescript Module
+# Twilio SMS Adapter
 
-So you want to create a reusable package that can be hosted on NPM and downloaded to projects to add value. Here are the steps and instructions you can take to create an effective package that can be reused accross backend projects.
-
-1. Click on the "Use this template"
-2. Clone the new repository that you make and start coding
+Simple function to send messages with twilio SID & token.
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 import Twilio from "twilio";
-import { SID, token } from "../env.js";
 
-export default function SMSAdapter(sid, token) {
+export default function SMSAdapter(sid: string, token: string, sender: number) {
   const client = Twilio(sid, token);
   return {
-    send: async ({ to, from, message }) => {
-      await client.messages.create({ body: message, to, from });
+    sender: "+1" + sender,
+    send: async (reciever: number, message: string) => {
+      await client.messages.create({
+        body: message,
+        to: "+1" + reciever,
+        from: "+1" + sender,
+      });
     },
   };
 }
-
-const adapter = SMSAdapter(SID, token);
-
-(async function () {
-  try {
-    const res = await adapter.send({
-      to: "+15417143099",
-      from: "+12083141442",
-      message: "This is a test message https://frm-staging.netlify.app",
-    });
-    console.log(res);
-  } catch (err) {
-    console.error(err);
-  }
-})();
